d3d212f8de48115ec4ee5d062a7fbd6b2c232044
Nandigam Shanmukanand <65825929+nandigam2005246@users.noreply.github.com>
2020-05-24 22:50:02 +0530

    Add files via upload

---
+++ dynamicmem.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 5
+int main()
+{
+    int i;
+//    int *arr = (int *) calloc (SIZE*sizeof(int));
+    // For calloc
+    int *arr = (int *) calloc (SIZE,sizeof(int));
+
+    for (i=0;i<5;i++)
+    scanf ("%d",&arr[i]);
+
+    for (i=0;i<5;i++)
+    printf ("%d\n",*(arr+i));
+
+    free(arr);
+
+    for (i=0;i<5;i++)
+    printf ("\n%d",arr[i]);
+
+
+    return 0;
+}
